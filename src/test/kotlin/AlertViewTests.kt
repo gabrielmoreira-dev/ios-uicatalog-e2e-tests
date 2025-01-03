d812cec9f7abf88658e048d910bfcc12d0307e83
Gabriel Moreira <g.alves.m.2008@gmail.com>
2025-01-02 21:41:49 -0300

    Added alert view tests

--- src/test/kotlin/AlertViewTests.kt
+++ src/test/kotlin/AlertViewTests.kt
@@ -1,9 +1,28 @@
 import io.appium.java_client.AppiumBy
+import org.testng.Assert
 import org.testng.annotations.Test
 
-class AlertViewTests: IOSBaseTest() {
+class AlertViewTests : IOSBaseTest() {
     @Test
     fun testAlertView() {
-        driver.findElement(AppiumBy.accessibilityId("Alert Views")).click()
+        driver.apply {
+            findElement(AppiumBy.accessibilityId("Alert Views")).click()
+            findElement(AppiumBy.iOSClassChain("**/XCUIElementTypeStaticText[`name == \"Text Entry\"`]")).click()
+            findElement(AppiumBy.iOSClassChain("**/XCUIElementTypeTextField")).sendKeys("Hello World")
+            findElement(AppiumBy.accessibilityId("OK")).click()
+        }
+    }
+
+    @Test
+    fun testClickOnCancelView() {
+        val text: String
+        driver.apply {
+            findElement(AppiumBy.accessibilityId("Alert Views")).click()
+            findElement(AppiumBy.accessibilityId("Confirm / Cancel")).click()
+            findElement(AppiumBy.iOSNsPredicateString("name BEGINSWITH[c] 'A message'")).let {
+                text = it.text
+            }
+        }
+        Assert.assertEquals(text, "A message should be a short, complete sentence.")
     }
 }

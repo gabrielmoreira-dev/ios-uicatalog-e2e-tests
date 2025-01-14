1aff4639569641ffc5431f62a80f5792a618f318
Gabriel Moreira <g.alves.m.2008@gmail.com>
2025-01-14 02:02:31 -0300

    Refactored BaseTest

--- src/test/kotlin/BaseTest.kt
+++ src/test/kotlin/BaseTest.kt
@@ -10,33 +10,34 @@ import java.net.URL
 import java.nio.file.Paths
 
 open class BaseTest {
+    companion object {
+        const val APP_DIR = "src/test/resources/UIKitCatalog.app"
+        const val SERVER_URL = "http://127.0.0.1:4723"
+    }
+
     private lateinit var service: AppiumDriverLocalService
     protected lateinit var driver: AppiumDriver
-
-    private val serverURL = URL("http://127.0.0.1:4723")
-    private val appPath = Paths.get(System.getProperty("user.dir"), "src/test/resources/UIKitCatalog.app")
-        .toAbsolutePath()
-        .toString()
 
     @BeforeSuite
     protected fun startAppium() {
         service = AppiumDriverLocalService.buildDefaultService().apply { start() }
     }
 
+    @AfterSuite
+    protected fun stopAppium() = service.stop()
+
     @BeforeClass
     protected fun setUpDriver() = DesiredCapabilities().apply {
+        val appPath = Paths.get(System.getProperty("user.dir"), APP_DIR).toAbsolutePath().toString()
         setCapability("appium:app", appPath)
         setCapability("platformName", "IOS")
         setCapability("appium:automationName", "XCUITest")
         setCapability("appium:deviceName", "iPhone 15 Pro")
         setCapability("appium:platformVersion", "18.0")
     }.let {
-        driver = IOSDriver(serverURL, it)
+        driver = IOSDriver(URL(SERVER_URL), it)
     }
 
     @AfterClass
     protected fun dispose() = driver.quit()
-
-    @AfterSuite
-    protected fun stopAppium() = service.stop()
 }

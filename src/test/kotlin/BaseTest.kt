d2573a0cbbfb0a73a051db9812c4652d49f08bcf
Gabriel Moreira <g.alves.m.2008@gmail.com>
2025-01-15 01:36:50 -0300

    Refactored capabilities builder

--- src/test/kotlin/BaseTest.kt
+++ src/test/kotlin/BaseTest.kt
@@ -1,7 +1,7 @@
 import io.appium.java_client.AppiumDriver
 import io.appium.java_client.ios.IOSDriver
+import io.appium.java_client.ios.options.XCUITestOptions
 import io.appium.java_client.service.local.AppiumDriverLocalService
-import org.openqa.selenium.remote.DesiredCapabilities
 import org.testng.annotations.AfterClass
 import org.testng.annotations.AfterSuite
 import org.testng.annotations.BeforeClass
@@ -11,7 +11,11 @@ import java.nio.file.Paths
 
 open class BaseTest {
     companion object {
+        const val AUTOMATION_NAME = "XCUITest"
         const val APP_DIR = "src/test/resources/UIKitCatalog.app"
+        const val PLATFORM_NAME = "IOS"
+        const val PLATFORM_VERSION = "18.0"
+        const val DEVICE_NAME = "iPhone 15 Pro"
         const val SERVER_URL = "http://127.0.0.1:4723"
     }
 
@@ -27,16 +31,13 @@ open class BaseTest {
     protected fun stopAppium() = service.stop()
 
     @BeforeClass
-    protected fun setUpDriver() = DesiredCapabilities().apply {
-        val appPath = Paths.get(System.getProperty("user.dir"), APP_DIR).toAbsolutePath().toString()
-        setCapability("appium:app", appPath)
-        setCapability("platformName", "IOS")
-        setCapability("appium:automationName", "XCUITest")
-        setCapability("appium:deviceName", "iPhone 15 Pro")
-        setCapability("appium:platformVersion", "18.0")
-    }.let {
-        driver = IOSDriver(URL(SERVER_URL), it)
-    }
+    protected fun setUpDriver() = XCUITestOptions()
+        .setAutomationName(AUTOMATION_NAME)
+        .setApp(Paths.get(System.getProperty("user.dir"), APP_DIR).toAbsolutePath().toString())
+        .setPlatformName(PLATFORM_NAME)
+        .setPlatformVersion(PLATFORM_VERSION)
+        .setDeviceName(DEVICE_NAME)
+        .let { driver = IOSDriver(URL(SERVER_URL), it) }
 
     @AfterClass
     protected fun dispose() = driver.quit()

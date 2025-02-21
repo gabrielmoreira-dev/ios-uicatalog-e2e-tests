1551cafe4f811037a7b46749ee5108ebb7bd9687
Gabriel Moreira <g.alves.m.2008@gmail.com>
2025-02-21 18:37:41 -0300

    feat: added driver wrapper

--- src/test/kotlin/BaseTest.kt
+++ src/test/kotlin/BaseTest.kt
@@ -1,24 +1,11 @@
 import io.appium.java_client.AppiumDriver
-import io.appium.java_client.ios.IOSDriver
-import io.appium.java_client.ios.options.XCUITestOptions
 import io.appium.java_client.service.local.AppiumDriverLocalService
-import org.testng.annotations.AfterClass
 import org.testng.annotations.AfterSuite
 import org.testng.annotations.BeforeClass
 import org.testng.annotations.BeforeSuite
-import java.net.URL
-import java.nio.file.Paths
+import utils.AppiumDriverWrapper
 
 open class BaseTest {
-    companion object {
-        const val AUTOMATION_NAME = "XCUITest"
-        const val APP_DIR = "src/test/resources/UIKitCatalog.app"
-        const val PLATFORM_NAME = "IOS"
-        const val PLATFORM_VERSION = "18.0"
-        const val DEVICE_NAME = "iPhone 15 Pro"
-        const val SERVER_URL = "http://127.0.0.1:4723"
-    }
-
     private lateinit var service: AppiumDriverLocalService
     protected lateinit var driver: AppiumDriver
 
@@ -27,18 +14,14 @@ open class BaseTest {
         service = AppiumDriverLocalService.buildDefaultService().apply { start() }
     }
 
-    @AfterSuite
-    protected fun stopAppium() = service.stop()
-
     @BeforeClass
-    protected fun setUpDriver() = XCUITestOptions()
-        .setAutomationName(AUTOMATION_NAME)
-        .setApp(Paths.get(System.getProperty("user.dir"), APP_DIR).toAbsolutePath().toString())
-        .setPlatformName(PLATFORM_NAME)
-        .setPlatformVersion(PLATFORM_VERSION)
-        .setDeviceName(DEVICE_NAME)
-        .let { driver = IOSDriver(URL(SERVER_URL), it) }
+    protected fun setUpClass() {
+        driver = AppiumDriverWrapper.getDriver()
+    }
 
-    @AfterClass
-    protected fun dispose() = driver.quit()
+    @AfterSuite
+    protected fun dispose() {
+        driver.quit()
+        service.stop()
+    }
 }
---
+++ src/test/kotlin/utils/AppiumDriverWrapper.kt
@@ -0,0 +1,34 @@
+package utils
+
+import io.appium.java_client.AppiumDriver
+import io.appium.java_client.ios.IOSDriver
+import io.appium.java_client.ios.options.XCUITestOptions
+import java.net.URL
+import java.nio.file.Paths
+
+class AppiumDriverWrapper private constructor() {
+    companion object {
+        private const val AUTOMATION_NAME = "XCUITest"
+        private const val USER_DIR = "user.dir"
+        private const val APP_DIR = "src/test/resources/UIKitCatalog.app"
+        private const val PLATFORM_NAME = "IOS"
+        private const val PLATFORM_VERSION = "18.0"
+        private const val DEVICE_NAME = "iPhone 15 Pro"
+        private const val SERVER_URL = "http://127.0.0.1:4723"
+
+        private var instance: AppiumDriver? = null
+
+        fun getDriver() = instance ?: synchronized(this) {
+            instance ?: getIOSDriver().also { instance = it }
+        }
+
+        private fun getIOSDriver(): IOSDriver = with(XCUITestOptions()) {
+            setAutomationName(AUTOMATION_NAME)
+            setApp(Paths.get(System.getProperty(USER_DIR), APP_DIR).toAbsolutePath().toString())
+            setPlatformName(PLATFORM_NAME)
+            setPlatformVersion(PLATFORM_VERSION)
+            setDeviceName(DEVICE_NAME)
+            return IOSDriver(URL(SERVER_URL), this)
+        }
+    }
+}
